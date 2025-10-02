16e326f4f633d14582011230de40193880925118
Carl-Be <63538326+Carl-Be@users.noreply.github.com>
2025-10-02 16:52:21 -0700

    Update nmapLikeOutput.py

--- nmapLikeOutput.py
+++ nmapLikeOutput.py
@@ -74,6 +74,9 @@ def main(task_id, report_id_arg):
         print("  No results.")
         gmp.disconnect()
         return
+    else:
+        with open(f"report_{report_id}.xml", "w") as file:
+            file.write(results)
 
     host_map = defaultdict(lambda: {'hostname': None, 'TCP': defaultdict(list), 'UDP': defaultdict(list)})
 
